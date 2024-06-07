60b13c7f12c121bd34cab1e65198638f4be46eab
sonormal <so.normal.email@gmail.com>
2024-06-07 14:06:49 +0200

    Update App.jsx

--- StoneworksApp/frontend/stoneworks/src/App.jsx
+++ StoneworksApp/frontend/stoneworks/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Main from './pages/Main';
 import Offers from './pages/Offers'; 
 import Header from './components/Header';
 import Registration from './pages/Registration';
+import Admin from './pages/Admin';
 import { UserProvider } from './context/UserContext';
+import UserContext from './context/UserContext';
+
+const PrivateRoute = ({ children, role, ...rest }) => {
+    const { user } = useContext(UserContext);
+    if (!user || user.type !== role) {
+        return <Navigate to="/login" />;
+    }
+    return children;
+};
 
 const App = () => {
     return (
@@ -13,11 +23,16 @@ const App = () => {
             <Router>
                 <Header />
                 <Routes>
-                  <Route path="/" element={<Main />} />
-                  <Route path="/main" element={<Main />} />
-                  <Route path="/offers" element={<Offers />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/registration" element={<Registration />} />
+                    <Route path="/" element={<Main />} />
+                    <Route path="/main" element={<Main />} />
+                    <Route path="/offers" element={<Offers />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/registration" element={<Registration />} />
+                    <Route path="/admin" element={
+                        <PrivateRoute role="admin">
+                            <Admin />
+                        </PrivateRoute>
+                    } />
                 </Routes>
             </Router>
         </UserProvider>
